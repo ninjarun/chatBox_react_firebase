c7e7358b0d25a52a1951b576b25751ee4ca58d69
yoni <orenyoni87@gmail.com>
2023-01-14 13:25:24 +0200

    displayusers update

--- src/components/DisplayUsers.jsx
+++ src/components/DisplayUsers.jsx
@@ -6,46 +6,54 @@ import CurrentUserContext from '../contexts/CurrentUserContext'
 const DisplayUsers = () => {
     const [Onlines, setOnlines] = useState([])
     const { setChatChooserChanger, CurrentUser } = useContext(CurrentUserContext)
-    const [CreateNewChatFlag, setCreateNewChatFlag] = useState(false)
+    const [CreateNewChatFlag, setCreateNewChatFlag] = useState(null)
 
 
     // creates table for both users private chat - NEEDS TO BE FIXED
     const handleClick = async (userClicked) => {
+        console.log(CreateNewChatFlag)
         setChatChooserChanger(userClicked)
         // query to get all messages that the current user is involved with
         const q = query(collection(db, "privateMsgs"), where("participants", "array-contains", CurrentUser.email));
-        console.log("we are here")
         const querySnapshot = await getDocs(q);
         let flag = false
-        // loop to get the exact conversation with other user
-        console.log( 'aaa',querySnapshot.length)
+        // loop to get the exact conversation with other user - ###################################################################################works well except the first run doesn't work!
         querySnapshot.forEach((doc) => {
+            console.log(doc.data())
+            console.log(userClicked)
             console.log('test if contains:', doc.data().participants.includes(userClicked));
-            flag=(doc.data().participants.includes(userClicked)) 
-            console.log('id',doc.id)
-            // (doc.data().participants.includes(userClicked)) && setCreateNewChatFlag(true)
-            // setCreateNewChatFlag(doc.data().participants.includes(userClicked))
+            if (doc.data().participants.includes(userClicked)) {
+                console.log('in if')
+                setCreateNewChatFlag(true)
+
+            }
+
+            //     flag=(doc.data().participants.includes(userClicked)) 
+            //     console.log(flag)
+            //     console.log('id',doc.id)
+            //     // (doc.data().participants.includes(userClicked)) && setCreateNewChatFlag(true)
+            //     // setCreateNewChatFlag(doc.data().participants.includes(userClicked))
         })
-
-
-
-
         console.log(CreateNewChatFlag)
-        // creates conversation if doesn't exist
-        if (!flag) {
-            console.log('yes')
-            const collectionRef = collection(db, "privateMsgs");
-            const payload = { "participants": [CurrentUser.email, userClicked] };
-            const docRef = await addDoc(collectionRef, payload);
-            console.log("The new ID is: " + docRef.id);
-            setCreateNewChatFlag(false)
-            console.log(CreateNewChatFlag)
 
-            } else {
-                console.log("created already")
-                setCreateNewChatFlag(false)
-        }
+
+
+
+        // // creates conversation if doesn't exist
+        // if (!flag) {
+        //     console.log('yes')
+        //     const collectionRef = collection(db, "privateMsgs");
+        //     const payload = { "participants": [CurrentUser.email, userClicked] };
+        //     const docRef = await addDoc(collectionRef, payload);
+        //     console.log("The new ID is: " + docRef.id);
+        //     console.log(CreateNewChatFlag)
+
+        // } else {
+        //     console.log("created already")
+        // }
     }
+
+
 
     // real time for online users list
     useEffect(() => {
